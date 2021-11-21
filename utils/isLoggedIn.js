2140fc5d37609a945fdec55ea1f2ac3a3d0e5926
Divyansh singh <singhdivyansh@gmail.com>
2021-11-21 13:49:44 +0000

    Add flash message

--- utils/isLoggedIn.js
+++ utils/isLoggedIn.js
@@ -2,6 +2,7 @@ const isLoggedIn = (req, res, next) => {
 	if (req.isAuthenticated()) {
 		return next();
 	}else {
+		req.flash("error","Hey! You Must be login to do that :D");
 		res.redirect('/login')
 	}
 }
